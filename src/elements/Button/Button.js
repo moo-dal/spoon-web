ae926f5615eaf8a2f10bd4a207a74365086b5813
MinhoCheon <kesakiyo@naver.com>
2017-08-24 21:15:07 +0900

    [수정] 버튼, 입력창 스타일 수정 (#9)

--- src/elements/Button/Button.js
+++ src/elements/Button/Button.js
@@ -1,22 +1,43 @@
 /* External dependencies */
 import React from 'react'
+import keyMirror from 'keymirror'
 import classNames from 'classnames'
 
 /* Internal dependencies */
 import styles from './Button.scss'
 
+const ButtonTypes = keyMirror({
+  DEFAULT: null,
+  BORDER: null,
+})
+
 class Button extends React.Component {
 
+  getButtonClassName(buttonType) {
+    switch (buttonType) {
+      case ButtonTypes.DEFAULT:
+        return styles.default
+
+      case ButtonTypes.BORDER:
+        return styles.border
+
+      default:
+        return styles.default
+    }
+  }
+
   render() {
-    const { className, children, ...props } = this.props
+    const { className, children, buttonType, ...props } = this.props
     return (
       <button
         {...props}
-        className={classNames(styles.button, className)}>
+        className={classNames(styles.button, this.getButtonClassName(buttonType), className)}>
         {children}
       </button>
     )
   }
 }
+
+Button.ButtonTypes = ButtonTypes
 
 export default Button
