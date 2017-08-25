003cc77734dbc3c2549ee86dbc3d0703bcaa6965
MinhoCheon <kesakiyo@naver.com>
2017-08-26 01:57:31 +0900

    [추가] 데일리 스케줄 입력 폼 반쯤 추가 (#14)

--- src/containers/DailySchedule/DailySchedule.js
+++ src/containers/DailySchedule/DailySchedule.js
@@ -1,6 +1,7 @@
 /* External dependencies */
 import React from 'react'
 import autobind from 'core-decorators/lib/autobind'
+import FontAwesome from 'react-fontawesome'
 import { connect } from 'react-redux'
 import { replace } from 'react-router-redux'
 
@@ -9,6 +10,7 @@ import styles from './DailySchedule.scss'
 import accountActions from '../../redux/actions/accountActions'
 import userSelector from '../../redux/selectors/userSelector'
 import UserInfo from '../../components/UserInfo'
+import CreateScheduleForm from '../../components/CreateScheduleForm'
 
 const mapStateToProps = (state) => ({
   user: userSelector.getUser(state),
@@ -17,10 +19,50 @@ const mapStateToProps = (state) => ({
 @connect(mapStateToProps)
 class DailySchedule extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {
+      showCreator: true,
+    }
+  }
+
   @autobind
   handleSignOut() {
     this.props.dispatch(accountActions.signOut())
     this.props.dispatch(replace('/signin'))
+  }
+
+  @autobind
+  handleShowCreator() {
+    this.setState({
+      showCreator: true,
+    })
+  }
+
+  @autobind
+  handleHideCreator() {
+    this.setState({
+      showCreator: false,
+    })
+  }
+
+  renderBody() {
+    if (this.state.showCreator) {
+      return (
+        <div className={styles.body}>
+          <CreateScheduleForm
+            onCancel={this.handleHideCreator} />
+        </div>
+      )
+    }
+    return (
+      <div className={styles.body}>
+        <div onClick={this.handleShowCreator} className={styles.creator}>
+          <FontAwesome name="plus" />
+          새로운 일정 등록하기
+        </div>
+      </div>
+    )
   }
 
   render() {
@@ -31,6 +73,7 @@ class DailySchedule extends React.Component {
         </div>
         <div className={styles.date}>14</div>
         <div className={styles.day}>TUESDAY</div>
+        {this.renderBody()}
       </div>
     )
   }
